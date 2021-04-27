59cb3036a606dd204e7d08efd1dfbaa84e30ace6
Mihajlo Strajin <strajin.mihajlo@uns.ac.rs>
2021-04-27 07:19:22 +0200

    Kreirane komponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HomeComponent } from './components/core/home/home.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { KreditComponent } from './components/kredit/kredit.component';
+import { KlijentComponent } from './components/klijent/klijent.component';
+import { RacunComponent } from './components/racun/racun.component';
+import { TipRacunaComponent } from './components/tip-racuna/tip-racuna.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthorComponent,
+    AboutComponent,
+    KreditComponent,
+    KlijentComponent,
+    RacunComponent,
+    TipRacunaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
