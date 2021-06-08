c24ce48734f9734b8c4b58c5c00145ff8d0981f7
Mihajlo Strajin <strajin.mihajlo@uns.ac.rs>
2021-06-08 11:52:27 +0200

    Test 2

--- Frontend_RVA/src/app/app.constants.ts
+++ Frontend_RVA/src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const KREDIT_URL = 'http://localhost:8083/kredit';
-export const TIP_RACUNA_URL = 'http://localhost:8083/tipRacuna';
-export const KLIJENT_URL = 'http://localhost:8083/klijent';
-export const RACUN_URL = 'http://localhost:8083/racun';
-export const RACUNI_ZA_KLIJENTA_URL = 'http://localhost:8083/racuniZaKlijenta';
+export const KREDIT_URL = 'https://strajin-rva-backend.herokuapp.com/kredit';
+export const TIP_RACUNA_URL = 'https://strajin-rva-backend.herokuapp.com/tipRacuna';
+export const KLIJENT_URL = 'https://strajin-rva-backend.herokuapp.com/klijent';
+export const RACUN_URL = 'https://strajin-rva-backend.herokuapp.com/racun';
+export const RACUNI_ZA_KLIJENTA_URL = 'https://strajin-rva-backend.herokuapp.com/racuniZaKlijenta';
